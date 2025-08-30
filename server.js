fc4386446abc6f5194a54289370f63de99a894c7
musab <syedmusab585@gmail.com>
2025-08-30 17:49:36 +0500

    change in server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 require("dotenv").config();
 
 // Import routes
@@ -12,10 +11,10 @@ const staffRoutes = require("./routes/staffRoutes");
 const settingsRoutes = require("./routes/settingsRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 // Middleware
 const allowedOrigins = [
-  "http://localhost:3000",    // Local development frontend
+  "http://localhost:3000", // Local development frontend
   "https://juddfront.vercel.app", // Your deployed frontend URL
 ];
 const corsOptions = {
@@ -26,7 +25,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Specify allowed methods
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
   credentials: true, // Allow cookies and authentication credentials to be sent
 };
 
@@ -37,12 +36,11 @@ app.use(express.json());
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose
   .connect(MONGODB_URI)
-  .then(async () => {
+  .then(() => {
     console.log("✅ Connected to MongoDB");
   })
   .catch((error) => {
     console.error("❌ MongoDB connection error:", error);
-    process.exit(1);
   });
 
 // API Routes
@@ -55,15 +53,10 @@ app.use("/api/courts", courtRoutes);
 app.use("/api/staff", staffRoutes);
 app.use("/api/settings", settingsRoutes);
 
-
+// Handle errors
 app.use((error, req, res, next) => {
   console.error("❌ Internal error:", error);
   res.status(500).json({ message: "Internal server error" });
-});
-
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
 
 // Export the handler for Vercel
